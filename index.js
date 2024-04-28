64942ef25f685dc25882c831baa85e85e23e9b95
Rishini <nawodirishini88@gmail.com>
2024-04-28 20:52:16 +0530

    Finalized

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  
 
 var typed = new Typed(".typing",{
-    strings:["","Web Designer" ,"Web Developper" , "Fullstack Developper","Freelancer"],
+    strings:["","Web Designer" ,"Web Developer" , "Fullstack Developer","Freelancer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
