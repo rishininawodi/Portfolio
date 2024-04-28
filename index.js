b3047f20b10e0e1e9263d155d75630513cc0fb52
Rishini <nawodirishini88@gmail.com>
2024-04-28 13:46:31 +0530

    Ifinished design part of my portfolio.And Finished Responsive part also.

---
+++ index.js
@@ -0,0 +1,25 @@
+/********************typing animation************ */
+ 
+
+var typed = new Typed(".typing",{
+    strings:["","Web Designer" ,"Web Developper" , "Fullstack Developper","Freelancer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
+
+/*****************side********************** */
+const nav = document.querySelector(".nav"),
+      navList = nav.querySelectorAll("li"),
+      totalNavList = navList.length;
+      for(let i=0;  i <totalNavList; i++){
+            
+            const a= navList[i].querySelector("a");
+
+            a.addEventListener("click" , function(){
+                for(let j=0; j<totalNavList; j++){
+                    navList[j].querySelector("a").classList.remove("active")
+                }
+                this.classList.add("active")
+            })
+      }
